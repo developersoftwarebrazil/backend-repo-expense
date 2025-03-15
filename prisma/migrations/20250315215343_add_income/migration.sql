0825329271827c5e9ccaf033df7880694fe4cef7
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-03-15 18:58:00 -0300

    adicionando o migrtion para o income

---
+++ prisma/migrations/20250315215343_add_income/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Income" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Income_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Income" ADD CONSTRAINT "Income_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
